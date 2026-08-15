e511dfa40fe21c76cd2689f237f4b5ceef51ad2b
kazi0515-gunner <kazi0515@outlook.jp>
2026-08-15 23:16:51 +0900

    Add files via upload

--- sw.js
+++ sw.js
@@ -1,4 +1,4 @@
-const CACHE = "squad-v26";
+const CACHE = "squad-v27";
 const ASSETS = ["./", "./index.html", "./manifest.webmanifest", "./icon-192.png", "./icon-512.png", "./apple-touch-icon.png"];
 self.addEventListener("install", (e) => {
   e.waitUntil(caches.open(CACHE).then((c) => c.addAll(ASSETS)).catch(() => {}));
